1e49bf6116ad4b58024ef08eacfe7e795de13f15
Teep-Boop <NiiktyAdeJ3125@gmail.com>
2023-11-24 23:25:16 -0300

    More stylization and organization. List of classes remade. Changes

--- Página Curso Matemática Básica/paginaCMB.js
+++ Página Curso Matemática Básica/paginaCMB.js
@@ -1,3 +1,12 @@
-function itensDaLista() {
-    
+function filterList() {
+  var search = document.getElementById("inputSearch").value.toLowerCase();
+  var items = document.getElementsByClassName("item")
+  for (var i = 0; i < items.length; i++) {
+    var itemText = items[i].textContent.toLowerCase();
+    if (itemText.includes(items)) {
+      items[i].style.display = 'block';
+    } else {
+      items[i].style.display = 'none';
+    }
+  }
 }
